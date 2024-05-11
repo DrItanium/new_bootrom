556416419f031b259bfd955c976321ef34a9e328
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-05-11 11:23:39 -0700

    Merge the logic for stage0 into the stage1 source file

--- stage1.s
+++ stage1.s
@@ -1,7 +1,17 @@
 /* Stage 1 Boot process, this is what is first run before going onto stage1 */
 
+.org 0x00000000
+    .word system_address_table # SAT pointer
+    .word prcb_ptr # prcb pointer
+    .word 0
+    .word stage1_entry # pointer to first ip
+    .word cs1 # calculated at link time (bind ?cs1 (- (+ ?SAT ?PRCB ?startIP)))
+    .word 0
+    .word 0
+    .word -1
 /* start in IO space */
 .org 0xFE400000
+.text
 /* NOTHING CAN COME between this and the org directive! */
 stage1_entry:
 	
@@ -30,3 +40,10 @@ sys_proc_table:
 .org 0xFE407000
 .align 6
 fault_proc_table:
+
+
+
+.org 0xFE40F000 /* constants */
+interpreter_passoff_address:
+	.word 0xFE800000
+
