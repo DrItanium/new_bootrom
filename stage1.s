0bdfd848921586c283fb80fb9d6bbcf6bd2e2ba0
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-05-11 11:44:26 -0700

    Put stage0 back in, it needs to be a separate thing

--- stage1.s
+++ stage1.s
@@ -1,19 +1,10 @@
 /* Stage 1 Boot process, this is what is first run before going onto stage1 */
 
-.org 0x00000000
-    .word system_address_table # SAT pointer
-    .word prcb_ptr # prcb pointer
-    .word 0
-    .word stage1_entry # pointer to first ip
-    .word cs1 # calculated at link time (bind ?cs1 (- (+ ?SAT ?PRCB ?startIP)))
-    .word 0
-    .word 0
-    .word -1
 /* start in IO space */
-.org 0xFE400000
-.text
+.section stage1_code,"x"
 /* NOTHING CAN COME between this and the org directive! */
 stage1_entry:
+	mov g0, g0
 	
 /* the structures here include
 
@@ -25,25 +16,15 @@ stage1_entry:
    - The interrupt table
 */
 
-.org 0xFE404000 /* all of the data tables start at 16k */
 .align 6
 system_address_table:
 
-.org 0xFE405000 /* each table then has a 4k offset from the previous one */
 .align 6
 prcb_ptr:
 
-.org 0xFE406000 
 .align 6
 sys_proc_table:
 
-.org 0xFE407000
 .align 6
 fault_proc_table:
-
-
-
-.org 0xFE40F000 /* constants */
-interpreter_passoff_address:
-	.word 0xFE800000
 
