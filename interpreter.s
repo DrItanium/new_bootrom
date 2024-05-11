7963376166eaf0d2decab658028b6eb988241d74
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-05-11 12:22:50 -0700

    Setup allocatable sections

--- interpreter.s
+++ interpreter.s
@@ -1,9 +1,12 @@
 /* initial boot setup */
-.section boot_words /* this will be at address zero encoded in to the 2560 itself */
+.global system_address_table
+.global prcb_ptr
+.global start_ip
+.section boot_words, "a" /* this will be at address zero encoded in to the 2560 itself */
     .word system_address_table # SAT pointer
     .word prcb_ptr # prcb pointer
     .word 0
-    .word stage1_entry # pointer to first ip
+    .word start_ip # pointer to first ip
 	.word cs1 # calculated at link time (bind ?cs1 (- (+ ?SAT ?PRCB ?startIP)))
     .word 0
     .word 0
@@ -11,7 +14,7 @@
 /* start in IO space */
 
 .section stage1_code,"x"
-stage1_entry:
+start_ip:
 	mov g0, g0
 	
 /* the structures here include
@@ -24,22 +27,27 @@ stage1_entry:
    - The interrupt table
 */
 
-.section stage1_sat
+.section stage1_sat, "a"
 .align 6
 system_address_table:
+	.word 0
 
-.section stage1_prcb
+.section stage1_prcb, "a"
 .align 6
 prcb_ptr:
+	.word 0
 
-.section stage1_sys_proc
+.section stage1_sys_proc, "a"
 .align 6
 sys_proc_table:
+	.word 0
 
-.section stage1_fault_proc
+.section stage1_fault_proc, "a"
 .align 6
 fault_proc_table:
+	.word 0
 
 .text
 .align 6
 interpreter_entry:
+	mov g0, g0
